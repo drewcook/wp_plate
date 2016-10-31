36aaa3c55229682295af547534fc212273109f2a
Drew Cook <altheawebservices@gmail.com>
2016-10-31 01:58:47 -0600

    Added in front page template

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying all pages.
+ * The template for displaying the front page.
  *
- * This is the template that displays all pages by default.
+ * This is the template that displays the front page.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
