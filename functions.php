e2517cf9eda07a5b8fbba4aac08c9ffe68959314
Drew Cook <altheawebservices@gmail.com>
2016-11-17 18:11:13 -0700

    Removed embed support

--- functions.php
+++ functions.php
@@ -68,9 +68,17 @@ remove_action( 'template_redirect', 'wp_shortlink_header' );
 remove_action( 'wp_head', 'rsd_link' );
 	// WP API Access - used by some plugins, but usually unneeded
 remove_action( 'wp_head', 'rest_output_link_wp_head');
-remove_action( 'wp_head', 'wp_oembed_add_discovery_links');
+	// Remove the REST API endpoint.
 remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+	// Turn off oEmbed auto discovery.
+add_filter( 'embed_oembed_discover', '__return_false' );
+	// Don't filter oEmbed results.
+remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+	// Remove oEmbed discovery links.
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+	// Remove oEmbed-specific JavaScript from the front-end and back-end.
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+	// Remove all embeds rewrite rules.
 add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
 
 /*
