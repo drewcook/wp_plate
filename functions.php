22f81c47a0206a13fbc122b7e865537d7f429228
Drew Cook <altheawebservices@gmail.com>
2016-11-26 15:14:38 -0700

    Removed 'disable_embeds_rewrites' filter

--- functions.php
+++ functions.php
@@ -78,8 +78,6 @@ remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 	// Remove oEmbed-specific JavaScript from the front-end and back-end.
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-	// Remove all embeds rewrite rules.
-add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
 
 /*
    Register Widgets
