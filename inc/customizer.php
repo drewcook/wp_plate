41940941c2df0c53d87fd92cd58f5c24cba81d33
Drew Cook <altheawebservices@gmail.com>
2017-03-28 22:55:41 -0600

    Remove customizer.js

--- inc/customizer.php
+++ inc/customizer.php
@@ -15,11 +15,3 @@ function aws_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 }
 add_action( 'customize_register', 'aws_customize_register' );
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function aws_customize_preview_js() {
-	wp_enqueue_script( 'aws_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-}
-add_action( 'customize_preview_init', 'aws_customize_preview_js' );
