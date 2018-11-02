e8cfeeb52a052cbb4b7cb4f27f8452f610eba3e4
Drew Cook <drew@linkrightmedia.com>
2018-11-01 22:14:27 -0600

    remove comments

--- page.php
+++ page.php
@@ -23,8 +23,6 @@ get_header(); ?>
 					get_breadcrumbs($post);
 					echo "</div>";
 					get_template_part( 'template-parts/content', 'page' );
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) : comments_template(); endif;
 				endwhile; ?>
 			</main><!-- #main -->
 		</div>
